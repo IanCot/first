ae4062cf951e7f2de91d9a70294525de73d24643
WojciechKot <gotter1985@gmail.com>
2022-05-21 16:46:20 +0200

    Fix local storage

--- src/App.js
+++ src/App.js
@@ -4,37 +4,38 @@ import TodoList from './TodoList';
 import './App.css';
 
 const LOCAL_STORAGE_KEY = 'Mytodos.todos';
+
 function App() {
 
-  const [todos,setTodo] = useState ([]);
-  const todoNameRef = useRef();
 
-  useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    if(storedTodos){
-      setTodo(storedTodos);
-    }
-  },[]);
+
+  const [todos,setTodo] = useState(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? []);
+  const todoNameRef = useRef();
 
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(todos));
   },[todos]);
+
+
   function togleTodo(id){
     const newTodos = [...todos];
     const todo = newTodos.find(todo => todo.id === id);
     todo.complete = !todo.complete;
     setTodo(newTodos);
   }
+
   function handleAddTodo(e) {
    const name = todoNameRef.current.value
     if(name === '') return;
     setTodo(prevTodos => [...prevTodos,{id:uuidv4(),name,complete:false}])
     todoNameRef.current.value = '';
   }
+  
   function handleClearTodos(){
     const newTodos = todos.filter(todo => !todo.complete);
     setTodo(newTodos);
   }
+  
   return (
       <>
       < TodoList todos = {todos} togleTodo={togleTodo} />
